c13251537db2733ab41325eca201086ac0f09677
Shadaf Hossain <shadafhossain01@gmail.com>
2025-11-27 23:29:57 +0600

    Route Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import General from "./components/General";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Shop from "./pages/shop/Shop";
+import Contact from "./pages/contact/Contact";
+
 const App = () => {
-  return <div>App</div>;
+  return <>
+    <Routes>
+      <Route element={<General/>}>
+      <Route index element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/shop" element={<Shop/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      </Route>
+    </Routes>
+  </>;
 };
 
 export default App;
