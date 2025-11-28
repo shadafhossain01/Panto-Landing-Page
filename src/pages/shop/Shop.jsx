9462787e5bf2f32842166b891dc5f5ab9e895321
Shadaf Hossain <shadafhossain01@gmail.com>
2025-11-29 02:18:43 +0600

    Update Shop.jsx

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,14 +1,20 @@
 import React from "react"; 
 import shopBg from "../../assets/banner.png"
+import ProductDisplay from "../../components/ProductDisplay";
 
 const Shop = () => {
   return (
-    <div
-      style={{ backgroundImage: `URL(${shopBg})` }}
-      className="w-full absolute top-0 h-[40vh] bg-center bg-no-repeat bg-cover"
-    >
-      <h2 className="text-white font-['Poppins'] text-center relative top-[60%] text-[42px]  font-bold">Shop Our Products</h2>
-    </div>
+    <>
+      <div
+        style={{ backgroundImage: `URL(${shopBg})` }}
+        className="w-full relative top-0 h-[45vh] bg-center bg-no-repeat bg-cover"
+      >
+        <h2 className="text-white font-['Poppins'] text-center relative top-[50%] text-[42px]  font-bold">
+          Shop Our Products
+        </h2>
+      </div>
+        <ProductDisplay heading={"What's Your Choice"} color={"#fff"} />
+    </>
   );
 };
 
