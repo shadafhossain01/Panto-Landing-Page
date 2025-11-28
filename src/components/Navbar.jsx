e965eeb6f21778d9375cb3ff8831e1d20eb9138f
Shadaf Hossain <shadafhossain01@gmail.com>
2025-11-28 17:20:43 +0600

    Navabr Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
 
           {/*  Dashboad Menu */}
           {/* Navlinks */}
-          <div className="hidden md:block">
+          <div className="hidden lg:block">
             {navPages.map((navItem) => (
               <NavLink
                 key={navItem.content}
@@ -74,7 +74,7 @@ const Navbar = () => {
           </div>
 
           {/*  Mobile Menu icon */}
-          <div className="cursor-pointer md:hidden">
+          <div className="cursor-pointer lg:hidden z-50">
             {menuOpen ? (
               <RxCross1
                 className="text-white text-2xl cursor-pointer"
@@ -91,7 +91,7 @@ const Navbar = () => {
           </div>
 
           {/*  Cart Icon */}
-          <div className="cursor-pointer relative hidden md:block">
+          <div className="cursor-pointer relative hidden lg:block">
             <Cart color={isScroll ? "black" : "white "} />
             <div className="w-[25px] h-[25px] bg-[#E58411] text-center rounded-full text-white absolute -right-[15px] top-px">
               5
@@ -100,25 +100,25 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Munu  */}
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <div
             className={`${
-              menuOpen ? "w-full h-full bg-[#000000d3] " : "w-0 h-full "
-            }  -z-50 transition-all  duration-700 ease-in-out transition-discrete fixed top-0  px-5 pt-35 left-0`}
+              menuOpen
+                ? "bg-[#000000d3] translate-x-0 "
+                : " -translate-x-full bg-[#000000d3]  "
+            }  z-40 transition-transform transform duration-200 fixed top-0  px-5 pt-35 left-0 w-full h-full`}
           >
-            {menuOpen && (
-              <div className="flex flex-col gap-y-10">
-                {navPages.map((navItem) => (
-                  <NavLink
-                    key={navItem.content}
-                    to={navItem.path}
-                    className="text-white font-black text-[18px]"
-                  >
-                    {navItem.content}
-                  </NavLink>
-                ))}
-              </div>
-            )}
+            <div className="flex flex-col gap-y-10">
+              {navPages.map((navItem) => (
+                <NavLink
+                  key={navItem.content}
+                  to={navItem.path}
+                  className="text-white font-black text-[18px]"
+                >
+                  {navItem.content}
+                </NavLink>
+              ))}
+            </div>
           </div>
         </div>
       </Container>
