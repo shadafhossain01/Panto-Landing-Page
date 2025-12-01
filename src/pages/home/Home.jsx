8886a119f4c89131c1bee40eb732c7b71ec2ce2d
Shadaf Hossain <shadafhossain01@gmail.com>
2025-12-01 22:16:38 +0600

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "./Hero";
 import Whyus from "./Whyus";
 import ProductDisplay from "../../components/ProductDisplay";
 import Content from "../../components/Content";
+import Testimonial from "../../components/Testimonial";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Whyus />
       <ProductDisplay heading={"Best Selling Product"} />
       <Content/>
+      <Testimonial/>
     </>
   );
 };
